6c84846ac4c19e9b52c1f5730d1a670aebe1b454
Pleuvens Fervil <pleuvens.fervil@gmail.com>
2023-10-05 01:41:21 +0200

    [TESTS] check that timeout happens on genserver radio player

--- test/radio/radio_player_test.exs
+++ test/radio/radio_player_test.exs
@@ -2,12 +2,17 @@ defmodule Radio.RadioPlayerTest do
   use Radio.DataCase
 
   setup do
-    RadioPlayer.start_link("test")
-    :ok
+    {_, pid} = RadioPlayer.start_link("test")
+    :erlang.trace(pid, true, [:receive])
+    {:ok, %{pid: pid}}
   end
 
   test "[RADIO PLAYER] get current song" do
     song = RadioPlayer.get_current_song("test")
     assert song.current_song == 0
+  end
+
+  test "[RADIO PLAYER] timeout callback triggered", %{pid: pid} do
+    assert_receive {:trace, ^pid, :receive, :timeout}, 2_000
   end
 end
